580244c6cccf5a793e153f0fffbd8054ac9328e3
ChrisKnowsDev <chrisharley81@gmail.com>
2021-02-16 18:07:03 -0500

    added border-right styling to list items

--- src/components/transaction.js
+++ src/components/transaction.js
@@ -1,12 +1,25 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 import React from 'react'
 
 const Transactions = ({ text, amount }) => {
   const sign = amount < 0 ? '-' : '+'
   return (
-    <li>
+    <li
+      className={amount < 0 ? 'minus' : 'plus'}
+      css={css`
+        .minus {
+          background: red;
+        }
+
+        .plus {
+          background: green;
+        }
+      `}
+    >
       {text}{' '}
       <span>
-        {sign}${amount}
+        {sign}${Math.abs(amount)}
       </span>
     </li>
   )
